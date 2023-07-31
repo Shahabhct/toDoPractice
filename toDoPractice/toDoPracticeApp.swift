661bdd49a0daec4908d91dd63e3ffe6e066461e5
Shahabhct <79043647+Shahabhct@users.noreply.github.com>
2023-07-31 09:08:40 +0400

    Initial Commit

---
+++ toDoPractice/toDoPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoPracticeApp.swift
+//  toDoPractice
+//
+//  Created by shahab on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
